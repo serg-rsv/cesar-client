f370aa0a8e651680ffbf492b3c31a207c97d5833
Serhii Rybak <rybaksw@gmail.com>
2023-04-21 22:27:46 +0300

    set live server for api

--- src/api/configAxios.ts
+++ src/api/configAxios.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:5000/api';
+// const BASE_URL = 'http://localhost:5000/api';
+const BASE_URL = 'https://cesar-ws.onrender.com/api';
 
 export const instance = axios.create({
   baseURL: BASE_URL,
